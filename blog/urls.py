f3f0937e482d2ecf05d92994394a6940579afd02
Atsuki Uchida <csatsfapsfu2930@gmail.com>
2022-06-09 11:18:34 +0900

    Finished deploying in cloud

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/',views.post_details,name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+
 ]
 
 
